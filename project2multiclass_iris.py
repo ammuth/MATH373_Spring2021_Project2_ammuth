efff69190ec437ee30b8d78002881a78fd05ef43
Annaliese Muth <61072717+ammuth@users.noreply.github.com>
2021-04-11 20:16:01 -0700

    Add files via upload

---
+++ project2multiclass_iris.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  2 11:40:20 2021
+
+@author: lngsm
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import random
+import seaborn
+from sklearn.datasets import load_iris
+import sklearn as sk
+import sklearn.model_selection
+
+#rom sklearn.datasets import load_iris
+iris = load_iris()
+X, y = iris.data[:-1,:], iris.target[:-1]
+X_train, X_test, Y_train, Y_test = sk.model_selection.train_test_split(X, y)
+
+
+def softmax(u):
+    expu = np.exp(u)
+    return expu/np.sum(expu)
+
+def crossEntropy(p, q):
+    return -np.vdot(p, np.log(q))
+
+def eval_L(X, Y, beta):
+    #Assume feature vectors are augmented already.
+    
+    N = X.shape[0]
+    L = 0.0
+    
+    for i in range(N):
+        XiHat = X[i]
+        Yi = Y[i]
+        qi = softmax(beta @ XiHat)
+        
+        L += crossEntropy(Yi, qi)
+        
+    return L
+
+def grad_L(beta, X, y):
+    #This function computes the gradient of L.
+    N = X.shape[0]
+    grad = 0
+    for i in range(N):
+        xiHat = X[i]
+        yi = y[i]
+        
+        yi_pred = softmax(np.vdot(xiHat, beta))
+        
+        grad = grad + (softmax(np.vdot(xiHat, beta)) - yi)*xiHat
+        
+        return grad/N
+    
+def gradient_descent(X, Y, alpha):
+    numEpochs = 100
+    N, d = X.shape
+    X = np.insert(X, 0, 1, axis = 1)
+    K = Y.shape[1]
+    
+    beta = np.zeros((K, d+1))
+    Lvals = []
+    
+    for ep in range(numEpochs):
+        
+        L = eval_L(X, Y, beta)
+        Lvals.append(L)
+        
+        print("Epoch is: " + str(ep) + " Cost is: " + str(L))
+        
+        #prm = np.random.permutation(N)
+        for i in range(N):
+            XiHat = X[i]
+            Yi = Y[i]
+            
+            qi = softmax(beta @ XiHat)
+            grad_Li = np.outer(qi-Yi, XiHat)
+            
+            beta = beta - alpha*grad_Li
+            
+    return beta/N, Lvals
+
+
+
+#N_train, numRows, numCols = X_train.shape
+#X_train = np.reshape(X_train, (N_train, numRows*numCols))
+
+
+Y_train = pd.get_dummies(Y_train).values
+
+alpha = 0.04
+
+
+beta, Lvals = gradient_descent(X_train, Y_train, alpha)
+
+
+plt.semilogy(Lvals)
+
+
+
+
+
+
+
+
+
+  
